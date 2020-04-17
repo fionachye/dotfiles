c1d400556d67ce0e514d05ae9899e52726a973b6
Fiona Chye <fiona_chye@hotmail.ca>
2020-04-16 22:54:13 -0400

    Update startup file

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get install -y curl gcc g++ make build-essential python-pip python3-pip python-setuptools python3-setuptools
+
 # Install latest stable nvim
 mkdir -p /tmp/_my_workbench_
 cd /tmp/_my_workbench_
@@ -13,16 +16,21 @@ cd ~/
 # Install nodejs and npm
 # Update according to this page: https://github.com/nodesource/distributions#debinstall
 # Install latest nodejs
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install npm packages
-npm install -g neovim
-npm install -g bash-language-server
+sudo npm install -g neovim
+sudo npm install -g bash-language-server
+
+sudo apt-get -y autoremove && sudo apt-get -y autoclean
 
 cd ~/
+python2 -m pip install --user --upgrade pip
+python2 -m pip install --user --upgrade pynvim
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user --upgrade pynvim
 
 echo "### Message to ME ###"
 echo "Ensure python or python3 is in your \$PATH."
-echo "Then run \`python3 -m pip install --user --upgrade pynvim\`"
-echo "Done. Remember to cd into dotfiles/ and copy vim configs"
+echo "Remember to cd into dotfiles/ and copy vim configs"
