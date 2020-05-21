68a31bb9756b7a0e9232406da683d7dd4a3342c9
Fiona Chye <fiona_chye@hotmail.ca>
2020-05-20 23:02:04 -0400

    Update installs and startup configs

--- arch-linux/startup.sh
+++ arch-linux/startup.sh
@@ -2,6 +2,11 @@
 sudo pacman -Syu
 sudo pacman -S sudo xorg-server lightdm lightdm-gtk-greeter openbox obconf pcmanfm tint2 nitrogen \
 xterm termite gnome-terminal tmux vim gnome-backgrounds menumaker python python2 fuse2 nodejs npm \
-base-devel
+base-devel ttf-dejavu ttf-liberation alsa-utils
 
 sudo systemctl enable lightdm.service
+
+# Generate config and start sound service
+alsactl store
+sudo systemctl enable alsa-restore.service
+sudo systemctl start alsa-restore.service
