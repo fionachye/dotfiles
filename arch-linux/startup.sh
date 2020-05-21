458b04e867792507ad4ca4eef1287c49b2f418ae
Fiona Chye <fiona_chye@hotmail.ca>
2020-05-20 21:56:35 -0400

    Update arch start up script

--- arch-linux/startup.sh
+++ arch-linux/startup.sh
@@ -1,6 +1,7 @@
 # Install sudo first
 sudo pacman -Syu
 sudo pacman -S sudo xorg-server lightdm lightdm-gtk-greeter openbox obconf pcmanfm tint2 nitrogen \
-xterm termite gnome-terminal tmux vim gnome-backgrounds menumaker python python2 fuse2 nodejs npm
+xterm termite gnome-terminal tmux vim gnome-backgrounds menumaker python python2 fuse2 nodejs npm \
+base-devel
 
 sudo systemctl enable lightdm.service
