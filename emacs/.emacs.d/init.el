7b3901d4e469e3a370990d58a627c5af5b9d6e77
Fiona Chye <fiona_chye@hotmail.ca>
2020-12-04 02:19:40 -0500

    Reorder file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,6 +21,7 @@
 ;; Disable automatic backups and autosaves
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(setq create-lockfiles nil)
 
 ;; Echo keystrokes after 0.1 seconds
 (setq echo-keystrokes 0.1)
@@ -37,22 +38,36 @@
 
 (use-package org
   :ensure t)
-
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
-
 (use-package powerline-evil
   :ensure t)
 (powerline-evil-vim-theme)
 (powerline-evil-vim-color-theme)
-
 (use-package key-chord
   :ensure t)
 (setq key-chord-two-keys-delay 0.1)
 (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
 (key-chord-mode 1)
+;; Themes
+;; Nord theme
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(use-package nord-theme
+   :config
+   (load-theme 'nord)
+   :ensure t)
+(use-package beacon ;; This applies a beacon effect to the highlighted line
+   :ensure t
+   :config
+   (beacon-mode 1))
+
+(use-package org-superstar  ;; Improved version of org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -68,32 +83,15 @@
  ;; If there is more than one, they won't work right.
  '(trailing-whitespace ((t (:background "dark red" :foreground "white")))))
 
-;; Themes
-;; Nord theme
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
-(use-package nord-theme
-   :config
-   (load-theme 'nord)
-   :ensure t)
 
-(global-hl-line-mode t) ;; This highlights the current line in the buffer
-
-(use-package beacon ;; This applies a beacon effect to the highlighted line
-   :ensure t
-   :config
-   (beacon-mode 1))
-
-(use-package org-superstar  ;; Improved version of org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
+;; Org Mode Settings
 (setq org-startup-indented t)           ;; Indent according to section
 (setq org-startup-with-inline-images t) ;; Display images in-buffer by default
 (setq org-hide-emphasis-markers t)      ;; Hide emphasis markers, like asterisks besides a bolded font
 
 ;; Basic Editor Settings
 (setq-default show-trailing-whitespace t)
-
+(global-hl-line-mode t) ;; This highlights the current line in the buffer
 
 ;; Evil key mappings
 (define-key evil-motion-state-map (kbd ":") 'evil-ex)
